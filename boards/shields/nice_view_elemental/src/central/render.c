6b650f5c9f07b9c5cb880e4c240f7df652115354
Ziembski <karolzurawinski@gmail.com>
2025-08-04 19:49:27 +0200

    Update render.c

--- boards/shields/nice_view_elemental/src/central/render.c
+++ boards/shields/nice_view_elemental/src/central/render.c
@@ -20,6 +20,7 @@
 #include "../../include/utils/draw_bluetooth_logo.h"
 #include "../../include/utils/draw_usb_logo.h"
 #include "../../include/utils/rotate_connectivity_canvas.h"
+#include "../../include/utils/rotate_main_canvas.h"
 
 void render_battery() {
     lv_canvas_fill_bg(battery_canvas, BACKGROUND_COLOR, LV_OPA_COVER);
