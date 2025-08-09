ed82a14ac0fba8cf8d2b45c95142799543bc8def
Ziembski <karolzurawinski@gmail.com>
2025-08-10 00:33:28 +0200

    Update render.c

--- boards/shields/nice_view_elemental/src/central/render.c
+++ boards/shields/nice_view_elemental/src/central/render.c
@@ -117,11 +117,6 @@ void render_main2() {
         text[strlen(states.layer.name)] = '\0';
     }
 
-    // Magic number. The height of the font from the baseline to the ascender
-    // height is 34px, but halving the space remaining of the full height gives
-    // us another value ((68px - 34px) / 2 = 17px). 
-    static const unsigned text_y_offset = 0;
-
     lv_draw_label_dsc_t layer_name_dsc;
     lv_draw_label_dsc_init(&layer_name_dsc);
     layer_name_dsc.color = FOREGROUND_COLOR;
@@ -131,8 +126,8 @@ void render_main2() {
     lv_canvas_draw_text(
         main2_canvas,
         0,
-        text_y_offset,
-        MAIN2_CANVAS_WIDTH,
+        0,
+        MAIN2_CANVAS_HEIGHT,
         &layer_name_dsc,
         text
     );
