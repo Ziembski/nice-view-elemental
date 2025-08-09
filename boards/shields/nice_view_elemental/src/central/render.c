eb82baa2a61c3c3c84f769c15ec485afdf7ac20e
Ziembski <karolzurawinski@gmail.com>
2025-08-10 01:54:11 +0200

    x

--- boards/shields/nice_view_elemental/src/central/render.c
+++ boards/shields/nice_view_elemental/src/central/render.c
@@ -33,12 +33,12 @@ void render_battery() {
 static void render_bluetooth_logo() {
     if (states.connectivity.active_profile_bonded) {
         if (states.connectivity.active_profile_connected) {
-            draw_bluetooth_logo(connectivity_canvas, 28, 4);
+            draw_bluetooth_logo(connectivity_canvas, 18, 14);
         } else {
-            draw_bluetooth_logo_outlined(connectivity_canvas, 28, 4);
+            draw_bluetooth_logo_outlined(connectivity_canvas, 18,  14);
         }
     } else {
-        draw_bluetooth_searching(connectivity_canvas, 28, 4);
+        draw_bluetooth_searching(connectivity_canvas, 18, 14);
     }
 }
 
